53469d479250fcd93c74ef3ec269197dde455693
for { kys() } <yyeet62425@gmail.com>
2024-12-31 21:08:51 +0530

    nice

--- init.lua
+++ init.lua
@@ -167,6 +167,11 @@ require('lazy').setup({
     },
   },
 
+  {
+    'wakatime/vim-wakatime',
+    lazy = false,
+  },
+
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
